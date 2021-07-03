a92fa13ca62be9f89f4fce0fdeaa155153d452ef
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-07-03 03:44:19 +0100

    wrote some notes

--- app/src/main/java/com/decagon/hilt/Note.kt
+++ app/src/main/java/com/decagon/hilt/Note.kt
@@ -13,3 +13,25 @@ has multiple bindings defined.
 either the application or the activity, Hilt provides the @ApplicationContext and @ActivityContext
 qualifiers.
  */
+
+/* Hilt doesn't generate a component for broadcast receivers because Hilt injects broadcast receivers
+directly from SingletonComponent.
+ */
+
+/* Hilt automatically creates and destroys instances of generated component classes following the
+lifecycle of the corresponding Android classes.
+ */
+
+/* By default, all bindings (@provides and @binds) in Hilt are unscoped. This means that each time your app requests the binding,
+Hilt creates a new instance of the needed type. */
+
+/* However, Hilt also allows a binding to be scoped to a particular component. Hilt only creates a
+scoped binding once per instance of the component that the binding is scoped to, and all requests for
+that binding share the same instance.
+ */
+
+/* Note: Scoping a binding to a component can be costly because the provided object stays in memory
+until that component is destroyed. Minimize the use of scoped bindings in your application.
+ */
+
+/* A binding's scope must match the scope of the component where it is installed. */
